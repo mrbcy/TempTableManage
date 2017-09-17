703b458cd6efd0425b3edc0a0d8963eff18c0fa4
mrbcy <yang@ricearmy.com>
2017-09-17 15:54:36 +0800

    修正权限认证bug

--- src/plugins/auth.js
+++ src/plugins/auth.js
@@ -3,18 +3,22 @@
  */
 exports.install = function (Vue, options) {
     Vue.prototype.hasPermission = function (userRoles, routeRoles){
-        for(let routeRole in routeRoles){
+        for(let i = 0; i < routeRoles.length; i++){
+            let routeRole = routeRoles[i];
             let isFind = false;
-            for(let userRole in userRoles){
+            for(let j = 0; j < userRoles.length; j++){
+                let userRole = userRoles[j];
                 if(userRole === routeRole){
                     isFind = true;
                 }
             }
             if(!isFind){
                 // 没找到要求的权限
+                console.log("false");
                 return false;
             }
         }
+        console.log("true");
         return true;
     };
 };
